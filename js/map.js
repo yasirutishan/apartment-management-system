80b0351eb4615ad2813c6317a9cba5a09c5e26de
yasirutishan <85503750+yasirutishan@users.noreply.github.com>
2022-04-22 19:10:42 +0530

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,20 @@
+function initialize() {
+	var myOptions = {
+		zoom: 15,
+		center: new google.maps.LatLng(6.905450, 79.850584), //change the coordinates
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		scrollwheel: false,
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+	var marker = new google.maps.Marker({
+		map: map,
+		position: new google.maps.LatLng(6.905450, 79.850584) //change the coordinates
+	});
+	google.maps.event.addListener(marker, "click", function() {
+		infowindow.open(map, marker);
+	});
+}
+google.maps.event.addDomListener(window, 'load', initialize);
